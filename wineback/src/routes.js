7f2634132c950a94ab75d554530f0c68808b44f3
Marius <grausas@gmail.com>
2020-12-01 21:08:13 +0200

    login added

--- wineback/src/routes.js
+++ wineback/src/routes.js
@@ -44,4 +44,29 @@ router.post("/register", middleware.validateUserData, (req, res) => {
   );
 });
 
+router.post("/login", (req, res) => {
+  const email = req.body.email.toLowerCase();
+  con.query(
+    `SELECT * FROM users WHERE email = ${mysql.escape(email)}`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res
+          .status(400)
+          .json({ msg: "Internal server error gathering user details" });
+      } else if (result.length !== 1) {
+        return res.status(400).json({
+          msg: "The provided details are incorrect or the user does not exist",
+        });
+      } else {
+        if (req.body.password !== result[0].password) {
+          res.status(400).json({ msg: "The provided password is incorrect" });
+        } else {
+          res.status(200).json({ msg: "Logged In" });
+        }
+      }
+    }
+  );
+});
+
 module.exports = router;
