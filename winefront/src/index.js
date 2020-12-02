db4e9f1096f0834df910b337756c57ef964de86e
Marius <grausas@gmail.com>
2020-12-02 18:06:19 +0200

    added auth context

--- winefront/src/index.js
+++ winefront/src/index.js
@@ -4,11 +4,14 @@ import Routes from "./Routes";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
 import "normalize.css";
+import AuthProvider from "./context/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
